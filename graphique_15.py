305b9008cedc6ab01c027fc52c223b6effa5cfcb
Louis GUENEGO <louis.guenego@gmail.com>
2025-05-08 17:49:13 +0200

    added graphique_15.py

---
+++ graphique_15.py
@@ -0,0 +1,36 @@
+import yfinance as yfinance
+import matplotlib.pyplot as pyplot
+
+gold = yfinance.download('GC=F', period="max")
+tbond = yfinance.download('ZN=F', period="max")
+
+goldvstbond = gold.div(tbond.rename(columns={'ZN=F': 'GC=F'}), fill_value=None).dropna()*36.98;
+
+goldvstbond['MA7an'] = goldvstbond['Close'].rolling(window=1750).mean()
+
+pyplot.figure(figsize=(8, 6))
+
+pyplot.plot(goldvstbond['Close'], color='red', label='Ratio or et marché obligataire US (base 100 au 21 septembre 2000)')
+pyplot.plot(goldvstbond['MA7an'], color='blue', label='Moyenne mobile à 7 ans du ratio')
+
+pyplot.title('Graphique n°15 - Règle de décision or ou marché obligataire')
+pyplot.legend()
+pyplot.grid(True)
+
+start = 0
+writing = 0
+for i in range( 0, len(goldvstbond)-1 ):
+        if writing:
+            if goldvstbond.iloc[i,5] > goldvstbond.iloc[i,0] :
+                pyplot.axvspan(goldvstbond.index[start], goldvstbond.index[i], facecolor='yellow', alpha=0.2)
+                writing = 0
+        else:
+            if goldvstbond.iloc[i,5] < goldvstbond.iloc[i,0] :  
+                start = i
+                writing = 1
+if writing == 1:
+    pyplot.axvspan(goldvstbond.index[start], goldvstbond.index[i], facecolor='yellow', alpha=0.2)
+
+print (goldvstbond['Close'].iloc[0]) # pour vérifier le premier point à la bonne date et à la base 100
+
+pyplot.show()
